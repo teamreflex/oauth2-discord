b1af06b8942fe8fbc25651f9ed6b78126dc1505f
David <david@team-reflex.com>
2016-04-09 08:50:22 -0400

    Applied fixes from StyleCI

--- src/Discord/Discord.php
+++ src/Discord/Discord.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is apart of the discord-oauth project.
+ *
+ * Copyright (c) 2016 David Cole <david@team-reflex.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the LICENSE.md file.
+ */
+
 namespace Discord\OAuth;
 
 use League\OAuth2\Client\Provider\AbstractProvider;
@@ -8,80 +17,78 @@ use Psr\Http\Message\ResponseInterface;
 
 class Discord extends AbstractProvider
 {
-	/**
-	 * The base API url.
-	 * 
-	 * @var string Base API url.
-	 */
-	const BASE_API_URL = 'https://discordapp.com/api';
+    /**
+     * The base API url.
+     *
+     * @var string Base API url.
+     */
+    const BASE_API_URL = 'https://discordapp.com/api';
 
-	/**
-	 * An array of available OAuth scopes.
-	 *
-	 * @var array Available scopes.
-	 */
-	protected $scopes = [
-		'identify', // Allows you to retrieve user data (except for email)
-		'email', // The same as identify but with email
-		'connections', // Allows you to retrieve connected YouTube and Twitch accounts
-		'guilds', // Allows you to retrieve the guilds the user is in
-		'guilds.join', // Allows you to join the guild for the user
-		'bot', // Defines a bot
-	];
+    /**
+     * An array of available OAuth scopes.
+     *
+     * @var array Available scopes.
+     */
+    protected $scopes = [
+        'identify', // Allows you to retrieve user data (except for email)
+        'email', // The same as identify but with email
+        'connections', // Allows you to retrieve connected YouTube and Twitch accounts
+        'guilds', // Allows you to retrieve the guilds the user is in
+        'guilds.join', // Allows you to join the guild for the user
+        'bot', // Defines a bot
+    ];
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function getBaseAuthorizationUrl()
-	{
-		return self::BASE_API_URL.'/oauth2/authorize';
-	}
+    /**
+     * {@inheritdoc}
+     */
+    public function getBaseAuthorizationUrl()
+    {
+        return self::BASE_API_URL.'/oauth2/authorize';
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function getBaseAccessTokenUrl(array $params)
-	{
-		return self::BASE_API_URL.'/oauth2/token';
-	}
+    /**
+     * {@inheritdoc}
+     */
+    public function getBaseAccessTokenUrl(array $params)
+    {
+        return self::BASE_API_URL.'/oauth2/token';
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function getResourceOwnerDetailsUrl(AccessToken $token)
-	{
-		return self::BASE_API_URL.'/users/@me';
-	}
+    /**
+     * {@inheritdoc}
+     */
+    public function getResourceOwnerDetailsUrl(AccessToken $token)
+    {
+        return self::BASE_API_URL.'/users/@me';
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	protected function getDefaultScopes()
-	{
-		return ['identify', 'email'];
-	}
+    /**
+     * {@inheritdoc}
+     */
+    protected function getDefaultScopes()
+    {
+        return ['identify', 'email'];
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function getScopeSeparator()
-	{
-		return ' ';
-	}
+    /**
+     * {@inheritdoc}
+     */
+    public function getScopeSeparator()
+    {
+        return ' ';
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	protected function checkResponse(ResponseInterface $response, $data)
-	{
-		
-	}
+    /**
+     * {@inheritdoc}
+     */
+    protected function checkResponse(ResponseInterface $response, $data)
+    {
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	protected function createResourceOwner(array $response, AccessToken $token)
-	{
-
-	}
+    /**
+     * {@inheritdoc}
+     */
+    protected function createResourceOwner(array $response, AccessToken $token)
+    {
+    }
 }
